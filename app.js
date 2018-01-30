efba5fbedff1dd6714b114e133d1aae64b25534c
Matthew Rizzini <matthewrizzini@gmail.com>
2018-01-30 19:50:13 +0000

    finished Code School Angular Assignment

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
 /* global angular $ */
 
-// express library
 
-// var express = require('express')
-// var app = express();
-
-// app.get("/", function(request, response) { // this line sends index.html to user
-//     response.sendFile(__dirname + "/public/index.html");   // __dirname = root directory of the app. 
-//     //if you make changes in here, you need to restart the server to see the updates
-// });
-
-//listen for incoming signals on this port.
-
-// app.listen(process.env.PORT);
 (function(){ // wrapping app in a closure. wrapping Javascript in a closure is a good habit
     var app = angular.module('store', [ ]);
     
@@ -20,29 +8,30 @@
         this.cars = carOptions; // sets carOptions to a property of the controller
     });
 
-      app.controller('PanelController', function(){
-      this.tab = 1;
+    app.controller('PanelController', function(){
+        this.tab = 1;
         
         this.selectTab = function(setTab) {
-          this.tab = setTab;  
+            this.tab = setTab;  
         };
         this.isSelected = function(checkTab) {
-          return this.tab === checkTab;  
-    
+            return this.tab === checkTab;  
     };
   });
 
+    app.controller('RentController', function (){
+   
+        this.rentCar = function(cars) {
+		if (cars.available > cars.rented) {
+			cars.rented = cars.rented + 1;
+			console.log(cars.rented);
+		} else if (cars.available == cars.rented) {
+			cars.soldOut = true;
+		}
+   };
+});
     
-    // app.controller('PanelController'), function() {
-    //     this.tab = 1;
-        
-    //     this.selectTab = function(setTab) {
-    //       this.tab = setTab;  
-    //     };
-    //     this.isSelected = function(checkTab) {
-    //       return this.tab === checkTab;  
-    //     };
-    // };
+
 
     var carOptions = [
     {
@@ -65,12 +54,26 @@
         price: 95,
         description: 'Great for showing off in that important business conference',
         canRent: true,
-        soldOut: true,
+        soldOut: false,
         available: 4,
         rented: 0,
         features: ["Backup Camera, ", "Heated Seats, ", "Wi-Fi, ", "GPS, ", "Bluetooth"],
         images: [
         "https://3.bp.blogspot.com/-ICvUYGYExCQ/WUfYNrddMWI/AAAAAAAA2ls/_pEabKoYQPgEGbzF-sHb6xAOefL7XqgWgCLcBGAs/s200-c/2018-Cadillac-XTS-001.jpg"    
+        ]
+    },
+    {
+        type: 'Motorcyle',
+        name: 'Harley Davidson',
+        price: 25,
+        description: 'An All-American classic bike',
+        canRent: true,
+        soldOut: false,
+        available: 8,
+        rented: 0,
+        features: ["Fuel Efficient, ", "Radio, ", "Chain Driven, "],
+        images: [
+        "https://gq-images.condecdn.net/image/L5OojerB2AK/crop/200/square/f/Harley-Davidson-GQ-13Nov17_b.jpg"    
         ]
     }
     ];
